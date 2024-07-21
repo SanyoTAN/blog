1c6feb97c02e8c2419bc3e0d0eb68dac6ea1ad5a
Administrator <1813115468@qq.com>
2024-07-21 19:17:12 +0800

    all

---
+++ src/components/Layout/SideBar/Author.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- 作者信息 -->
+    <div class="author-container">
+<!--      &lt;!&ndash; 作者头像 &ndash;&gt;-->
+<!--      <img class="author-avatar" :src="blog.blogInfo.siteConfig.authorAvatar" />-->
+<!--      &lt;!&ndash; 作者名称 &ndash;&gt;-->
+<!--      <p class="author-name">{{ blog.blogInfo.siteConfig.siteAuthor }}</p>-->
+      <!-- 网站简介 -->
+      <div class="site-desc">博客介绍</div>
+    </div>
+    <!-- 博客信息 -->
+    <blog-info></blog-info>
+    <!-- 社交 -->
+    <social-list></social-list>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useBlogStore } from "@/store";
+const blog = useBlogStore();
+
+</script>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,51 @@
+import dayjs from "dayjs";
+import { fileURLToPath } from "node:url";
+import { defineConfig, loadEnv } from "vite";
+// import legacy from "@vitejs/plugin-legacy";
+import {
+	createViteProxy,
+	setupVitePlugins
+} from "./build";
+export default defineConfig((configEnv) => {
+	const viteEnv = loadEnv(
+		configEnv.mode,
+		process.cwd()
+	) as Env.ImportMeta;
+
+	const buildTime = dayjs().format("YYYY-MM-DD HH:mm:ss");
+
+	return {
+		base: viteEnv.VITE_BASE_URL,
+		resolve: {
+			alias: {
+				"~": fileURLToPath(new URL("./", import.meta.url)),
+				"@": fileURLToPath(new URL("./src", import.meta.url)),
+			},
+		},
+		define: {
+			__VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+			BUILD_TIME: JSON.stringify(buildTime),
+		},
+		plugins: [
+			setupVitePlugins(viteEnv),
+			// legacy({
+			// 	targets: ['ie>=11'],
+			// 	additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+			// })
+		],
+		server: {
+			host: "0.0.0.0",
+			port: 1314,
+			open: true,
+			proxy: createViteProxy(viteEnv),
+		},
+		build: {
+			reportCompressedSize: false,
+			sourcemap: false,
+			outDir: viteEnv.VITE_DIST_NAME,
+			commonjsOptions: {
+				ignoreTryCatch: false,
+			},
+		},
+	};
+});
